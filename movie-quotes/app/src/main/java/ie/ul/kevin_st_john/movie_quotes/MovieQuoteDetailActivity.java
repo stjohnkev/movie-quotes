39ab99f4200449014f95d129196c7d7cbeeca640
stjohnkev <43213852+stjohnkev@users.noreply.github.com>
2018-11-19 22:44:36 +0000

    created activity

---
+++ movie-quotes/app/src/main/java/ie/ul/kevin_st_john/movie_quotes/MovieQuoteDetailActivity.java
@@ -0,0 +1,29 @@
+package ie.ul.kevin_st_john.movie_quotes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MovieQuoteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_quote_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
